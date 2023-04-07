d90636b4142d46395c4237bea3cda8bc9c62e19c
YALIM GÜRBÜZ <yalim1999@hotmail.com>
2023-04-07 15:29:22 +0300

    React Native Redux and Hooks Tutorial

---
+++ App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
+import FoodForm from "./src/foodForm";
+import FoodList from "./src/foodList";
+import { NavigationContainer } from "@react-navigation/native";
+
+const Stack = createStackNavigator();
+
+export default AppStack = () => (
+  <NavigationContainer>
+    <Stack.Navigator>
+      <Stack.Screen
+        name="FoodForm"
+        component={FoodForm}
+        options={{
+          title: "Cheetah Coding",
+          headerTintColor: "orange",
+          headerStyle: {
+            backgroundColor: "black",
+          },
+        }}
+      />
+      <Stack.Screen
+        name="FoodList"
+        component={FoodList}
+        options={{
+          headerTintColor: "orange",
+          headerStyle: {
+            backgroundColor: "black",
+          },
+        }}
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
---
+++ src/foodForm.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  TextInput,
+  Text,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { useDispatch } from "react-redux";
+import { addFood } from "./actions/food";
+
+const FoodForm = ({ navigation }) => {
+  const [food, setFood] = useState("");
+
+  const dispatch = useDispatch();
+
+  const submitFood = (food) => dispatch(addFood(food));
+
+  return (
+    <View style={styles.container}>
+      <Image style={styles.image} source={require("./assets/logo.png")} />
+      <Text style={styles.title}>YLMGRBZ REDUX TUTORİAL</Text>
+      <TextInput
+        value={food}
+        placeholder="Name"
+        style={styles.foodInput}
+        onChangeText={(food) => setFood(food)}
+      />
+      <TouchableOpacity
+        style={{ marginBottom: 16 }}
+        onPress={() => {
+          submitFood(food);
+          setFood("");
+        }}
+      >
+        <Text style={{ fontSize: 22, color: "#5fc9f8" }}>Submit</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        style={{ marginBottom: 16 }}
+        onPress={() => navigation.navigate("FoodList")}
+      >
+        <Text style={{ fontSize: 22, color: "white" }}>Go to FoodList</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#212121",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  title: {
+    fontSize: 48,
+    marginBottom: 30,
+    marginTop: 16,
+    color: "white",
+  },
+  foodInput: {
+    fontSize: 24,
+    marginBottom: 32,
+    borderWidth: 1,
+    padding: 12,
+    width: "80%",
+    borderRadius: 10,
+    backgroundColor: "white",
+  },
+  image: {
+    width: 120,
+    height: 120,
+    borderColor: "orange",
+    borderWidth: 2,
+    borderRadius: 100,
+  },
+});
+
+export default FoodForm;
